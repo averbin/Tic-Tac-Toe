29974565b8fd2bc7a7651791769850428d6ed32e
averbin <aleksander.verbin@scaletools.com>
2018-04-24 20:34:21 +0300

    Moved items elements to ItemInterface module.

---
+++ ItemInterface.lua
@@ -0,0 +1,82 @@
+
+ItemInterface = {}
+ItemInterface.mt = {}
+
+setmetatable(ItemInterface, ItemInterface.mt)
+
+function CalculatePoints(elements)
+  local firstPoint = { x = 0 , y = 0 }
+  local secondPoint = { x = 0, y = 0 }
+  local rangeByX = elements[#elements].element.x - elements[1].element.x
+  local rangeByY = elements[#elements].element.y - elements[1].element.y
+  if rangeByX ~= 0 and rangeByY == 0 then
+    -- count horizontal.
+    firstPoint.x = elements[1].element.x - elements[1].element.width / 2
+    firstPoint.y = elements[1].element.y
+    secondPoint.x = elements[#elements].element.x + elements[1].element.width / 2
+    secondPoint.y = elements[#elements].element.y
+  elseif rangeByY ~= 0 and rangeByX == 0 then
+    -- count vertical.
+    firstPoint.x = elements[1].element.x
+    firstPoint.y = elements[1].element.y - elements[1].element.height / 2
+    secondPoint.x = elements[#elements].element.x
+    secondPoint.y = elements[#elements].element.y + elements[1].element.height / 2
+  else
+    -- count other.
+    if elements[1].element.x < elements[#elements].element.x then
+      -- leftTop -> bottomRight.
+      firstPoint.x = elements[1].element.x - elements[#elements].element.width / 2
+      firstPoint.y = elements[1].element.y - elements[#elements].element.height / 2
+      secondPoint.x = elements[#elements].element.x + elements[#elements].element.width / 2
+      secondPoint.y = elements[#elements].element.y + elements[#elements].element.height / 2
+    else
+      -- rightTop -> bottomLeft.
+      firstPoint.x = elements[1].element.x + elements[1].element.width / 2
+      firstPoint.y = elements[1].element.y - elements[1].element.height / 2
+      secondPoint.x = elements[#elements].element.x - elements[#elements].element.width / 2
+      secondPoint.y = elements[#elements].element.y + elements[#elements].element.height / 2
+    end
+  end
+  return firstPoint, secondPoint
+end
+
+function ItemInterface:DrawEx(group, xPos, yPos)
+  --xLines = {{},{}}
+  local range = 20
+  local firstLine = display.newLine(group, xPos - range, yPos - range, xPos + range, yPos + range)
+  firstLine.strokeWidth = 2
+  local secondLine = display.newLine(group, xPos + range, yPos - range, xPos - range, yPos + range)
+  secondLine.strokeWidth = 2
+  table.insert(self, firstLine)
+  table.insert(self, secondLine)
+end
+
+function ItemInterface:DrawCircle(group, xPos, yPos)
+  local circle = display.newCircle(group, xPos, yPos, 20 )
+  circle:setFillColor( 0.2 )
+  circle.strokeWidth = 2
+  circle:setStrokeColor( 1, 1, 1 )
+  table.insert(self, circle)
+end
+
+function ItemInterface:DrawTheLine(group, elements)
+  if elements ~= nil and #elements ~= o then
+    local startPoint , endPoint = CalculatePoints(elements)
+    local lineForWin = display.newLine(group, startPoint.x, startPoint.y,
+                                      endPoint.x, endPoint.y)
+    lineForWin.strokeWidth = 2
+    table.insert(self, lineForWin)
+  else
+    error("Error, the are no elements at all!")
+  end
+end
+
+function ItemInterface:RemoveAllItems()
+  for i = #self, 1, -1 do
+    object = self[i]
+    display.remove(object)
+    table.remove( self, i )
+  end
+end
+
+return ItemInterface
--- main.lua
+++ main.lua
@@ -5,13 +5,13 @@
 -----------------------------------------------------------------------------------------
 local Board = require "Board"
 local BoardElement = require "BoardElement"
+local itemsInterface = require "ItemInterface"
 local elementsGroup = display.newGroup()
 local itemsGroup = display.newGroup()
 local uiGroup = display.newGroup()
 local run = true
-
-local items = {}
 local turn = "x" -- could be "x" or "o"
+
 local exitButton = display.newRect(
   display.contentWidth - display.contentWidth / 2,
   display.contentHeight - 20 - 30,
@@ -26,89 +26,14 @@ local textTurn = display.newText(uiGroup,
    native.systemFont, 18)
 
 function RetryTapEvent( event )
-  RemoveAllItems()
+  itemsInterface:RemoveAllItems()
   Board:CleanBoard()
   turn = "x"
   run = true
-  textTurn.text = "Turn: " .. turn
+  textTurn.text = "Turn: "  .. turn
   --os.exit()
 end
 exitButton:addEventListener("tap", RetryTapEvent)
-
-function DrawCircle(xPos, yPos)
-  local circle = display.newCircle(itemsGroup, xPos, yPos, 20 )
-  circle:setFillColor( 0.2 )
-  circle.strokeWidth = 2
-  circle:setStrokeColor( 1, 1, 1 )
-  table.insert(items, circle)
-end
-
-function DrawEx(xPos, yPos)
-  --xLines = {{},{}}
-  local range = 20
-  local firstLine = display.newLine(itemsGroup, xPos - range, yPos - range, xPos + range, yPos + range)
-  firstLine.strokeWidth = 2
-  local secondLine = display.newLine(itemsGroup, xPos + range, yPos - range, xPos - range, yPos + range)
-  secondLine.strokeWidth = 2
-  table.insert(items, firstLine)
-  table.insert(items, secondLine)
-end
-
-function RemoveAllItems()
-  for i = #items, 1, -1 do
-    object = items[i]
-    display.remove(object)
-    table.remove( items, i )
-  end
-end
-
-function CalculatePoints(elements)
-  local firstPoint = { x = 0 , y = 0 }
-  local secondPoint = { x = 0, y = 0 }
-  local rangeByX = elements[#elements].element.x - elements[1].element.x
-  local rangeByY = elements[#elements].element.y - elements[1].element.y
-  if rangeByX ~= 0 and rangeByY == 0 then
-    -- count horizontal.
-    firstPoint.x = elements[1].element.x - elements[1].element.width / 2
-    firstPoint.y = elements[1].element.y
-    secondPoint.x = elements[#elements].element.x + elements[1].element.width / 2
-    secondPoint.y = elements[#elements].element.y
-  elseif rangeByY ~= 0 and rangeByX == 0 then
-    -- count vertical.
-    firstPoint.x = elements[1].element.x
-    firstPoint.y = elements[1].element.y - elements[1].element.height / 2
-    secondPoint.x = elements[#elements].element.x
-    secondPoint.y = elements[#elements].element.y + elements[1].element.height / 2
-  else
-    -- count other.
-    if elements[1].element.x < elements[#elements].element.x then
-      -- leftTop -> bottomRight.
-      firstPoint.x = elements[1].element.x - elements[#elements].element.width / 2
-      firstPoint.y = elements[1].element.y - elements[#elements].element.height / 2
-      secondPoint.x = elements[#elements].element.x + elements[#elements].element.width / 2
-      secondPoint.y = elements[#elements].element.y + elements[#elements].element.height / 2
-    else
-      -- rightTop -> bottomLeft.
-      firstPoint.x = elements[1].element.x + elements[1].element.width / 2
-      firstPoint.y = elements[1].element.y - elements[1].element.height / 2
-      secondPoint.x = elements[#elements].element.x - elements[#elements].element.width / 2
-      secondPoint.y = elements[#elements].element.y + elements[#elements].element.height / 2
-    end
-  end
-  return firstPoint, secondPoint
-end
-
-function DrawTheLine(elements)
-  if elements ~= nil and #elements ~= o then
-    local startPoint , endPoint = CalculatePoints(elements)
-    local lineForWin = display.newLine(itemsGroup, startPoint.x, startPoint.y,
-                                      endPoint.x, endPoint.y)
-    lineForWin.strokeWidth = 2
-    table.insert(items, lineForWin)
-  else
-    error("Error, the are no elements at all!")
-  end
-end
 
 function tapEvent( event )
   if run == false then
@@ -120,11 +45,11 @@ function tapEvent( event )
     print("Turn: " .. turn)
     if turn == "x" then
       boardElement.mark = "x"
-      DrawEx(event.target.x, event.target.y)
+      itemsInterface:DrawEx(itemsGroup, event.target.x, event.target.y)
       turn = "o"
     else
       boardElement.mark = "o"
-      DrawCircle(event.target.x, event.target.y)
+      itemsInterface:DrawCircle(itemsGroup, event.target.x, event.target.y)
       turn = "x"
     end
   else
@@ -134,7 +59,7 @@ function tapEvent( event )
   local markWinRow = Board:FindByHorizontal()
   if markWinRow ~= nil and markWinRow ~= 0 then
     textTurn.text = "Won: " .. markWinRow[1].mark
-    DrawTheLine(markWinRow)
+    itemsInterface:DrawTheLine(itemsGroup, markWinRow)
     run = false
     return true
   end
@@ -142,7 +67,7 @@ function tapEvent( event )
   local markWinColumn = Board:FindByVertical()
   if markWinColumn ~= nil and #markWinColumn ~= 0 then
     textTurn.text = "Won: " .. markWinColumn[1].mark
-    DrawTheLine(markWinColumn)
+    itemsInterface:DrawTheLine(itemsGroup, markWinColumn)
     run = false
     return true
   end
@@ -150,7 +75,7 @@ function tapEvent( event )
   local markWinLeftTop = Board:FindFromLeftToptoRightBottom()
   if markWinLeftTop ~= nil and #markWinLeftTop ~= 0 then
     textTurn.text = "Won: " .. markWinLeftTop[1].mark
-    DrawTheLine(markWinLeftTop)
+    itemsInterface:DrawTheLine(itemsGroup, markWinLeftTop)
     run = false
     return true
   end
@@ -158,7 +83,7 @@ function tapEvent( event )
   local markWinRightTop = Board:FindFromRightToptoBottomLeft()
   if markWinRightTop ~= nil and #markWinRightTop ~= 0 then
     textTurn.text = "Won: " .. markWinRightTop[1].mark
-    DrawTheLine(markWinRightTop)
+    itemsInterface:DrawTheLine(itemsGroup, markWinRightTop)
     run = false
     return true
   end
